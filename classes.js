82bdb4678b156496a2d1c9a4a7979e2b61ad9abf
tharun05 <tharundillikar@gmail.com>
2018-04-05 11:15:28 +0530

    classes and access specifiers

---
+++ classes.js
@@ -0,0 +1,46 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person(name, username) {
+        this.name = name;
+        this.username = username;
+    }
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
+    Person.company = "HCL";
+    return Person;
+}());
+var employee = new Person('tharun', 'dillikar');
+employee.setType('developer');
+console.log(Person.company);
+var Persontwo = /** @class */ (function (_super) {
+    __extends(Persontwo, _super);
+    function Persontwo() {
+        return _super.call(this, 'arun', 'dillikar') || this;
+    }
+    return Persontwo;
+}(Person));
+var personthree = /** @class */ (function (_super) {
+    __extends(personthree, _super);
+    function personthree() {
+        return _super.call(this) || this;
+    }
+    return personthree;
+}(Persontwo));
+var persontwo = new Persontwo();
+console.log(persontwo);
+//# sourceMappingURL=classes.js.map
---
+++ classes.ts
@@ -0,0 +1,37 @@
+class Person{
+    name:string;
+    private type:string;
+    username:string;
+    static company:string = "HCL";
+    protected age:number
+    constructor(name:string,username:string){
+        this.name = name;
+        this.username = username;
+    }
+    printAge(){
+        console.log(this.age);
+    }
+    setType(type:string){
+        this.type = type;
+        console.log(this.type)
+    }
+}
+
+let employee = new Person('tharun','dillikar');
+employee.setType('developer');
+console.log(Person.company);
+
+class Persontwo extends Person{
+    constructor(){
+        super('arun','dillikar');
+     
+    }
+}
+class personthree extends Persontwo{
+    constructor(){
+        super();
+      
+    }
+}
+let persontwo = new Persontwo();
+console.log(persontwo);
